459aa98241e589d5086b0c0420ade8f2ce8f90eb
刘禹辰 <yuchen@10-209-123-10.dynapool.shanghai.nyu.edu>
2021-05-13 11:28:13 +0800

    Add usage

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# tic-tac-toe_Q-learning
+# RL-tictactoe
 
-TODO: Descriptions
+I have implemented two algorithms for the tic-tac-toe games: TD(0) & Q-Learning
+For the final question, we only need TD(0) to do model-free prediction. Due to the lateness of the message in the slack, I have already done the self-play implementation so that I get ultimate value for each state. 
+
+## Usage
+See all the usage in help function in details
+```
+python3 main.py -h
+```
+
+play games with the trained agent
+```
+python3 main.py -p
+```
+
+show first three state values(this is used for the final question)
+```
+python3 main.py -o
+```
--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def Debug(func):
         func()
 
 
-##########TD LEARNING############
+##########TD(0) LEARNING############
 agent1 = TDAgent(1)
 agent0 = TDAgent(0)
 
@@ -52,20 +52,78 @@ def TDcheck_probabilities(board, agent1):
         ls[i] = agent1.Q.get(tuple(temp),-1)
     return ls
 
-while True:
-    board.reset()
+def play(board, agent1):
     while True:
-        print(TDcheck_probabilities(board, agent1))
-        action1 = agent1.act(board.state)
-        next_state,reward,gameover = board.TDstep(action1, 1)
-        board.render()
-        if gameover: break 
-        p = int(input("Place ur step:(0-9)"))
-        board.state[p] = 0
-        board.render()
-        board.gamecheck()
-        if gameover: break
-        
+        board.reset()
+        while True:
+            print(TDcheck_probabilities(board, agent1))
+            action1 = agent1.act(board.state)
+            next_state,reward,gameover = board.TDstep(action1, 1)
+            board.render()
+            if gameover: break 
+            p = int(input("Place ur step:(0-9)"))
+            board.state[p] = 0
+            board.render()
+            board.gamecheck()
+            if gameover: break
+
+# play(board, agent1)
+
+
+
+import getopt, sys
+ 
+ 
+# Remove 1st argument from the
+# list of command line arguments
+argumentList = sys.argv[1:]
+ 
+# Options
+options = "hmpo"
+ 
+# Long options
+long_options = ["Help", "My_file", "Output", "Play"]
+ 
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+     
+    # checking each argument
+    for currentArgument, currentValue in arguments:
+ 
+        if currentArgument in ("-h", "--Help"):
+            msg = \
+            '''
+            optional arguments:
+                -h, --Help: show help messages
+                -m, --My_file: show the name of the file
+                -p, --Play: play with the agent
+                -o, --Output: show value for first three states
+            '''
+            print (msg)
+             
+        elif currentArgument in ("-m", "--My_file"):
+            print ("Displaying file_name:", sys.argv[0])
+             
+        elif currentArgument in ("-o", "--Output"):
+            state1 = [-1,-1,-1,-1,-1,-1,-1,-1,-1]
+            state2 = [1,-1,-1,-1,-1,-1,-1,-1,-1]
+            state3 = [1,-1,-1,-1,0,-1,-1,-1,-1]
+
+            print(agent1.Q[tuple(state1)])
+            print(agent1.Q[tuple(state2)])
+            print(agent1.Q[tuple(state3)])
+
+        elif currentArgument in ('-p', "--Play"):
+            play(board, agent1)
+             
+except getopt.error as err:
+    # output error, and return with an error code
+    print (str(err))
+
+
+
+
     
 
 
